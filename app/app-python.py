73381fc743c04043cf1d8d5d35c0a1b9ffe369c9
Léa <140352752+LH-90@users.noreply.github.com>
2023-07-27 11:24:46 +0100

    Update app-python.py

--- app/app-python.py
+++ app/app-python.py
@@ -5,12 +5,42 @@ import dash
 from dash import Dash, html, dcc
 
 #Dataset importation 1
-
+df= pd.read_csv("Methane_emissions_cleaned.csv")
 #Dataset importation 2
 
 #Dataset importation 3
 
 #Figure1
+
+country_names = df["country"]
+values = df["emissions"]
+segments=df['segment']
+
+data = {
+    "Country": country_names,
+    "Emissions": values,
+    "Segment":segments
+}
+
+custom_color_scale = [
+    (0.0, 'green'),   # For the minimum value (0.0), use green color
+    (0.2, 'yellow'),   # For the minimum value (0.2), use yellow color
+    (0.5, 'orange'),  # For the middle value (0.5), use orange color
+    (1.0, 'red')     # For the maximum value (1.0), use red color
+]
+
+# Create the choropleth map using Plotly Express
+
+fig1 = px.choropleth(data_frame=data,
+                    locations="Country",
+                    locationmode="country names",
+                    color="Emissions",
+                    color_continuous_scale=custom_color_scale,
+                    projection="natural earth")
+
+# Set the title
+
+fig1.update_layout(title="IEA estimates for methane emissions (kilotonnes) by country in 2022")
 
 #figure 2
 
