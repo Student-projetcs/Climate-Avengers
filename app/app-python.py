48eb3aae6f5c462fca5780aa1e7c6b9c13f01afa
harrybj777 <138459326+harrybj777@users.noreply.github.com>
2023-07-27 11:54:29 +0100

    Update app-python.py
    added code for Line_Graph (figure/dataset 3)

--- app/app-python.py
+++ app/app-python.py
@@ -9,7 +9,7 @@ from dash import Dash, html, dcc
 #Dataset importation 2
 
 #Dataset importation 3
-
+df=pd.read_csv("OneColumnTempChange.csv")
 #Figure1
 
 #figure 2
@@ -27,8 +27,37 @@ app.layout = html.Div([
         dcc.Graph()
         ]),
     html.Div([
-        html.H1("Figure 3"),
-        dcc.Graph()
+        
+        html.H1('Temperature Changes Over Time'),
+        dcc.Graph(id="graph"),
+        dcc.Checklist(
+            id="checklist",
+            options=["Asia", "Europe", "Africa","Americas","Oceania",'Global Avg'],
+            value=[ "Global Avg"],
+            inline=True
+        ),
+    
+])
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("checklist", "value"))
+    
+def update_line_chart(continents):
+
+    mask = df.Continent.isin(continents)
+    fig = px.line(df[mask], x=df[mask]['Year'], y=df[mask]['Temp Change'], color=df[mask]['Continent'])
+
+
+    fig.update_layout(
+    xaxis_title="Year",
+    yaxis_title="Temperature Change",
+    legend_title="",
+   )
+
+    return fig
+
+        
         ]),
      html.Div([
         html.H1("Figure 4"),
